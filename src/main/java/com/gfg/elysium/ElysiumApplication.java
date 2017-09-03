27341681fa991aa386805ff61e700d85d69f8349
rogueagent <piyushbhawan@gmail.com>
2017-09-03 11:54:19 +1000

    mvn project init

---
+++ src/main/java/com/gfg/elysium/ElysiumApplication.java
@@ -0,0 +1,12 @@
+package com.gfg.elysium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElysiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElysiumApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gfg/elysium/ElysiumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gfg.elysium;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElysiumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
